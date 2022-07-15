7cc13998e58b92a7f7290ad08277e4ca7edf9159
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-15 16:31:42 -0500

    Updated Files.
    Finally got it working.

--- Dice.gd
+++ Dice.gd
@@ -13,7 +13,7 @@ func _ready():
 	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
 	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
 	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
-	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .2]))
+	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
 
 
 func draw_roll(values, weights):
@@ -24,25 +24,25 @@ func draw_roll(values, weights):
 		totalWeight += n
 	if totalWeight != 1:
 		rng.randomize()
-		return rng.randi_range(0, 6)
+		return rng.randi_range(0, 6) # this works
 	else:
 		roll = rng.randf_range(0,1)
 		print(roll, " ROLL")
-		if roll >= values[0] + values[1] + values[2] + values[3] + values[4]:
+		if roll >= weights[0] + weights[1] + weights[2] + weights[3] + weights[4]:
 			print("got heeeere")
 			rvalue = values[5]
 			return rvalue
-		elif roll >= values[0] + values[1] + values[2] + values[3]:
+		elif roll >= weights[0] + weights[1] + weights[2] + weights[3]:
 			print("got here")
 			rvalue = values[4]
 			return rvalue
-		elif roll >= values[0] + values[1] + values[2]:
+		elif roll >= weights[0] + weights[1] + weights[2]:
 			rvalue = values[3]
 			return rvalue
-		elif roll >= values[0] + values[1]:
+		elif roll >= weights[0] + weights[1]:
 			rvalue = values[2]
 			return rvalue
-		elif roll >= values[0] + values[1]:
+		elif roll >= weights[0] + weights[1]:
 			rvalue = values[1]
 			return rvalue
 		elif roll >= 0:
