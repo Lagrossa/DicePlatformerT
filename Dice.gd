47d691b6977d27f860a75387a8aa842b597bfdd1
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-15 16:34:32 -0500

    Updated Files.

--- Dice.gd
+++ Dice.gd
@@ -1,22 +1,9 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var rng = RandomNumberGenerator.new()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
-	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
-	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
-	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
-	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
-	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
 
-
-func draw_roll(values, weights):
+func draw_roll(values, weights): # Array Values, Array Weights
 	var totalWeight = 0
 	var roll
 	var rvalue
@@ -29,11 +16,9 @@ func draw_roll(values, weights):
 		roll = rng.randf_range(0,1)
 		print(roll, " ROLL")
 		if roll >= weights[0] + weights[1] + weights[2] + weights[3] + weights[4]:
-			print("got heeeere")
 			rvalue = values[5]
 			return rvalue
 		elif roll >= weights[0] + weights[1] + weights[2] + weights[3]:
-			print("got here")
 			rvalue = values[4]
 			return rvalue
 		elif roll >= weights[0] + weights[1] + weights[2]:
