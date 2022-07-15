a74cd1e37ae896ef897bbc43b4ea76cd72a8b4e1
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-15 16:26:43 -0500

    Updated Files.

--- Dice.gd
+++ Dice.gd
@@ -13,7 +13,7 @@ func _ready():
 	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
 	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
 	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
-	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .1]))
+	print(draw_roll([1,2,3,4,5,6],[.1, .2, .3, .2, .1, .2]))
 
 
 func draw_roll(values, weights):
@@ -23,22 +23,29 @@ func draw_roll(values, weights):
 	for n in weights:
 		totalWeight += n
 	if totalWeight != 1:
-		rng.randi_range(0, values[values.size()-1])
+		rng.randomize()
+		return rng.randi_range(0, 6)
 	else:
-		roll = rng.randf()
-		if roll < values[0]:
-			rvalue = values[0]
-		elif roll < values[1]:
-			rvalue = values[1]
-		elif roll < values[2]:
-			rvalue = values[2]
-		elif roll < values[3]:
-			rvalue = values[3]
-		elif roll < values[4]:
-			rvalue = values[4]
-		elif roll < values[5]:
+		roll = rng.randf_range(0,1)
+		print(roll, " ROLL")
+		if roll >= values[0] + values[1] + values[2] + values[3] + values[4]:
+			print("got heeeere")
 			rvalue = values[5]
-		elif roll < values[6]:
-			rvalue = values[6]
+			return rvalue
+		elif roll >= values[0] + values[1] + values[2] + values[3]:
+			print("got here")
+			rvalue = values[4]
+			return rvalue
+		elif roll >= values[0] + values[1] + values[2]:
+			rvalue = values[3]
+			return rvalue
+		elif roll >= values[0] + values[1]:
+			rvalue = values[2]
+			return rvalue
+		elif roll >= values[0] + values[1]:
+			rvalue = values[1]
+			return rvalue
+		elif roll >= 0:
+			rvalue = values[0]
 	print("rolled ", rvalue)
 	return rvalue
