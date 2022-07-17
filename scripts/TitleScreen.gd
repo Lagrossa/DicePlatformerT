e3e4489cc73f4ef503f81f65f32077ef7a65fb0a
Karagaya <99910080+Karagaya@users.noreply.github.com>
2022-07-17 21:08:10 +0200

    titlescreen

---
+++ scripts/TitleScreen.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://Scenes/mainScenes/MainScene.tscn")
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	pass # Replace with function body.
