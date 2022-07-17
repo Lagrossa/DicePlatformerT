33314bae9c307e4960c5f389bc06d507fc89ca2d
Lagrosa <65159359+Lagrossa@users.noreply.github.com>
2022-07-17 10:47:04 -0500

    Updated branches (#6)
    * some envoirement and stuff
    
    * Updated. Script works.
    
    * All the buffs working.
    
    Got buffs working. Need an enemy spawner
    
    Co-authored-by: Karagaya <99910080+Karagaya@users.noreply.github.com>

--- scripts/Dice.gd
+++ scripts/Dice.gd
@@ -2,31 +2,48 @@ extends Node
 class_name Dice
 var rng = RandomNumberGenerator.new()
 onready var player = get_tree().get_nodes_in_group("player")[0]
-
+var jh = funcref(self, "jump_height")
+var db = funcref(self, "damage_buff")
+var td = funcref(self, "take_damage")
+var hd = funcref(self, "take_damage")
+var se = funcref(self, "spawn_enemy")
 
 func dice_ability(value):
 	var dice_to_ability = {
-		1 : "add_life", 
-		2 : jump_height(),
-		3 : "increase_damage",
-		4 : "spawn_enemy",
-		5 : "damage_player",
-		6 : jump_height(),
+		1 : jh, # jump
+		2 : db,
+		3 : td,
+		4 : hd,
+		5 : se,
+		6 : jh,
 	}
-#	match value: # I'm not sure if you can call functions from dictionaries in gdscript. If it doesn't work just use this
-#		1:
-#			print("lol")
-#		2:
-#			 player.JumpHeight
-#		_:
-#			player.JumpHeight = 500
-	return dice_to_ability[value]
+
+	return dice_to_ability[value].call_func()
 
 
 func jump_height():
-	player.JumpHeight += 50
-	
+	var jumps = rng.randi_range(1,6)
+	print("jumps", jumps)
+	player.JumpAmount = jumps
 
+func damage_buff():
+	var damage = rng.randi_range(1,3)
+	print("player damage", damage)
+	player.Damage += damage
+	
+func take_damage():
+	var damage = rng.randi_range(1,3)
+	print("take damage", damage)
+	player.Health -= damage
+	
+func heal_damage():
+	var damage = rng.randi_range(1,6)
+	print("heal damage", damage)
+	player.Health += damage
+
+func spawn_enemy():
+	#no enemy spawning code yet..
+	print("would spawn enemy")
 func draw_roll(values, weights): # Array Values, Array Weights
 	var totalWeight = 0
 	var roll
